6d5441ed334e5ec493bc35bd955693e4918fd267
Paul Young <paulyoungonline@gmail.com>
2016-10-23 00:45:56 -0700

    Add IsForeign instance for ModuleName

--- src/CoreFn/ModuleName.purs
+++ src/CoreFn/ModuleName.purs
@@ -2,12 +2,17 @@ module CoreFn.ModuleName
   ( ModuleName(..)
   ) where
 
+import Data.Foreign (readString)
+import Data.Foreign.Class (class IsForeign)
 import Data.Generic (class Generic, gCompare, gEq, gShow)
-import Prelude (class Eq, class Ord, class Show)
+import Prelude ((<$>), class Eq, class Ord, class Show)
 
 newtype ModuleName = ModuleName String
 
 derive instance genericModuleName :: Generic ModuleName
+
+instance isForeignModuleName :: IsForeign ModuleName where
+  read value = ModuleName <$> readString value
 
 instance showModuleName :: Show ModuleName where
   show = gShow
