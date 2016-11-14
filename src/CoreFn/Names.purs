e14270cffd2544d93475e15c5007a5758ba394d4
Paul Young <paulyoungonline@gmail.com>
2016-11-13 22:58:03 -0800

    Add `Qualified`

--- src/CoreFn/Names.purs
+++ src/CoreFn/Names.purs
@@ -2,12 +2,20 @@ module CoreFn.Names
   ( ModuleName(..)
   , OpName(..)
   , ProperName(..)
+  , Qualified(..)
   ) where
 
 import Prelude
-import Data.Foreign (readString)
+import Control.Error.Util (exceptNoteM)
+import Data.Array (init, last, null)
+import Data.Foreign (readString, ForeignError(ForeignError))
 import Data.Foreign.Class (class IsForeign)
 import Data.Generic (class Generic, gCompare, gEq, gShow)
+import Data.List.NonEmpty (singleton)
+import Data.List.Types (NonEmptyList)
+import Data.Maybe (Maybe(..))
+import Data.Newtype (class Newtype, wrap)
+import Data.String (Pattern(..), joinWith, split)
 
 -- |
 -- Module names
@@ -34,6 +42,7 @@ instance ordModuleName :: Ord ModuleName where
 newtype OpName = OpName String
 
 derive instance genericOpName :: Generic OpName
+derive instance newtypeOpName :: Newtype OpName _
 
 instance isForeignOpName :: IsForeign OpName where
   read value = OpName <$> readString value
@@ -54,6 +63,7 @@ instance ordOpName :: Ord OpName where
 newtype ProperName = ProperName String
 
 derive instance genericProperName :: Generic ProperName
+derive instance newtypeProperName :: Newtype ProperName _
 
 instance isForeignProperName :: IsForeign ProperName where
   read value = ProperName <$> readString value
@@ -67,3 +77,43 @@ instance eqProperName :: Eq ProperName where
 instance ordProperName :: Ord ProperName where
   compare = gCompare
 
+-- |
+-- A qualified name, i.e. a name with an optional module name
+--
+data Qualified a = Qualified (Maybe ModuleName) a
+
+derive instance genericQualified :: (Generic a) => Generic (Qualified a)
+
+instance isForeignQualified :: Newtype t String => IsForeign (Qualified t) where
+  read value = readString value >>= (flip exceptNoteM errors) <<< toQualified
+
+    where
+
+    arrayToMaybe :: forall a. Array a -> Maybe (Array a)
+    arrayToMaybe xs | null xs = Nothing
+                    | otherwise = Just xs
+
+    init' :: forall a. Array a -> Maybe (Array a)
+    init' xs = init xs >>= arrayToMaybe
+
+    delimiter = "."
+
+    toModuleName :: Array String -> ModuleName
+    toModuleName = ModuleName <<< (joinWith delimiter)
+
+    toQualified :: Newtype t String => String -> Maybe (Qualified t)
+    toQualified s = do
+      let parts = split (Pattern delimiter) s
+      Qualified (toModuleName <$> init' parts) <<< wrap <$> last parts
+
+    errors :: NonEmptyList ForeignError
+    errors = singleton (ForeignError "Error parsing qualified name")
+
+instance showQualified :: (Generic a, Show a) => Show (Qualified a) where
+  show = gShow
+
+instance eqQualified :: (Generic a, Eq a) => Eq (Qualified a) where
+  eq = gEq
+
+instance ordQualified :: (Generic a, Ord a) => Ord (Qualified a) where
+  compare = gCompare
--- test/CoreFn/Names.purs
+++ test/CoreFn/Names.purs
@@ -7,11 +7,12 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (log, CONSOLE)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Except.Trans (ExceptT)
-import CoreFn.Names (ModuleName(..), OpName(..), ProperName(..))
+import CoreFn.Names (ModuleName(..), OpName(..), ProperName(..), Qualified(..))
 import Data.Foreign (ForeignError)
 import Data.Foreign.Class (readJSON)
 import Data.Identity (Identity)
 import Data.List.Types (NonEmptyList)
+import Data.Maybe (Maybe(..))
 import Test.Util (assertEqual, expectSuccess)
 
 testNames :: forall e. Eff (console :: CONSOLE, err :: EXCEPTION | e) Unit
@@ -22,6 +23,10 @@ testNames = do
   testModuleName
   testOpName
   testProperName
+  testQualifiedOpNameWithoutModuleName
+  testQualifiedOpNameWithModuleName
+  testQualifiedProperNameWithoutModuleName
+  testQualifiedProperNameWithModuleName
 
   where
 
@@ -69,3 +74,58 @@ testNames = do
 
     expectSuccess description result \(x) ->
       assertEqual x (ProperName "Nothing")
+
+  -- |
+  -- Qualified
+  --
+  testQualifiedOpNameWithoutModuleName = do
+    let description = "Qualified OpName without module name from JSON results in success"
+
+    let json = """
+      "bind"
+    """
+
+    let result = readJSON json :: ExceptT (NonEmptyList ForeignError) Identity (Qualified OpName)
+
+    expectSuccess description result \(Qualified x y) -> do
+      assertEqual x Nothing
+      assertEqual y (OpName "bind")
+
+  testQualifiedOpNameWithModuleName = do
+    let description = "Qualified OpName with module name from JSON results in success"
+
+    let json = """
+      "Control.Bind.bind"
+    """
+
+    let result = readJSON json :: ExceptT (NonEmptyList ForeignError) Identity (Qualified OpName)
+
+    expectSuccess description result \(Qualified x y) -> do
+      assertEqual x (Just $ ModuleName "Control.Bind")
+      assertEqual y (OpName "bind")
+
+  testQualifiedProperNameWithoutModuleName = do
+    let description = "Qualified ProperName without module name from JSON results in success"
+
+    let json = """
+      "Nothing"
+    """
+
+    let result = readJSON json :: ExceptT (NonEmptyList ForeignError) Identity (Qualified ProperName)
+
+    expectSuccess description result \(Qualified x y) -> do
+      assertEqual x Nothing
+      assertEqual y (ProperName "Nothing")
+
+  testQualifiedProperNameWithModuleName = do
+    let description = "Qualified ProperName with module name from JSON results in success"
+
+    let json = """
+      "Data.Maybe.Nothing"
+    """
+
+    let result = readJSON json :: ExceptT (NonEmptyList ForeignError) Identity (Qualified ProperName)
+
+    expectSuccess description result \(Qualified x y) -> do
+      assertEqual x (Just $ ModuleName "Data.Maybe")
+      assertEqual y (ProperName "Nothing")
