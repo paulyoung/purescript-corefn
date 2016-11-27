1571ca6fcca728e97ceb64ac7b4a3e80f2be5db3
Paul Young <paulyoungonline@gmail.com>
2016-11-26 17:49:29 -0800

    Add literals

--- test/Main.purs
+++ test/Main.purs
@@ -4,6 +4,7 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (log, CONSOLE)
 import Control.Monad.Eff.Exception (EXCEPTION)
+import Test.CoreFn.Expr (testExpr, testLiterals)
 import Test.CoreFn.Ident (testIdent)
 import Test.CoreFn.Module (testModule)
 import Test.CoreFn.Names (testNames)
@@ -12,5 +13,7 @@ main :: forall e. Eff (console :: CONSOLE, err :: EXCEPTION | e) Unit
 main = do
   testIdent
   testNames
+  testLiterals
+  testExpr
   testModule
   log ""
