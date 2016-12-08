1636f69eb79ee20ba2f9e93bad8c97f3bd15ad97
Paul Young <paulyoungonline@gmail.com>
2016-12-07 23:09:20 -0800

    Refactor `readModule`

--- src/CoreFn/Module.purs
+++ src/CoreFn/Module.purs
@@ -4,6 +4,7 @@ module CoreFn.Module
   ) where
 
 import Prelude
+import Data.Foreign.Keys as K
 import Data.Array as Array
 import Control.Error.Util (exceptNoteA)
 import Control.Monad.Except.Trans (ExceptT)
@@ -12,7 +13,6 @@ import CoreFn.Names (ModuleName(..))
 import Data.Foreign (F, Foreign, ForeignError(..), parseJSON)
 import Data.Foreign.Class (readProp)
 import Data.Foreign.Index (prop)
-import Data.Foreign.Keys (keys)
 import Data.Generic (gShow, class Generic)
 import Data.Identity (Identity(..))
 import Data.List.NonEmpty (singleton)
@@ -37,12 +37,13 @@ instance showModule :: Show Module where
 
 readModule :: Foreign -> F Module
 readModule x = do
-  let key = firstKey x
-  value <- key >>= (flip prop) x
+  keys <- K.keys x
+  key <- head keys
+  value <- prop key x
   moduleExports <- readProp "exports" value
   moduleForeign <- readProp "foreign" value
   moduleImports <- readProp "imports" value
-  moduleName <- ModuleName <$> key
+  let moduleName = ModuleName key
 
   pure $ Module
     { moduleExports: moduleExports
@@ -56,9 +57,6 @@ readModule x = do
   head :: forall a. Array a -> ExceptT (NonEmptyList ForeignError) Identity a
   head y = exceptNoteA ((Identity <<< Array.head) y)
                         (singleton (ForeignError "Module name not found"))
-
-  firstKey :: Foreign -> ExceptT (NonEmptyList ForeignError) Identity String
-  firstKey y = keys y >>= head
 
 readModuleJSON :: String -> F Module
 readModuleJSON json = parseJSON json >>= readModule
