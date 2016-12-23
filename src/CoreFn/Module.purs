afe80feeea19b06faec537b0a5a8ba735c650d4b
Paul Young <paulyoungonline@gmail.com>
2016-12-23 15:56:03 +0000

    Add module declarations

--- src/CoreFn/Module.purs
+++ src/CoreFn/Module.purs
@@ -5,36 +5,47 @@ module CoreFn.Module
   ) where
 
 import Prelude
+import CoreFn.Expr (Bind, readBind)
 import CoreFn.Ident (Ident, readIdent)
 import CoreFn.Names (ModuleName(..), readModuleName)
 import CoreFn.Util (objectProp)
 import Data.Foreign (F, Foreign, parseJSON, readArray)
 import Data.Foreign.Class (readProp)
-import Data.Generic (gShow, class Generic)
 import Data.Foreign.Index (class Index)
 import Data.Traversable (traverse)
 
 -- |
 -- The CoreFn module representation
 --
-data Module = Module
-  { moduleExports :: Array Ident
+data Module a = Module
+  { moduleDecls :: Array (Bind a)
+  , moduleExports :: Array Ident
   , moduleForeign :: Array Ident
   , moduleImports :: Array ModuleName
   , moduleName :: ModuleName
   }
 
-derive instance eqModule :: Eq Module
-derive instance genericModule :: Generic Module
-derive instance ordModule :: Ord Module
+derive instance eqModule :: Eq a => Eq (Module a)
+derive instance ordModule :: Ord a => Ord (Module a)
 
-instance showModule :: Show Module where
-  show = gShow
+instance showModule :: Show a => Show (Module a) where
+  show (Module { moduleDecls
+               , moduleExports
+               , moduleForeign
+               , moduleImports
+               , moduleName }) =
+       "(Module { moduleName: " <> show moduleName <>
+               ", moduleDecls: " <> show moduleDecls <>
+               ", moduleExports: " <> show moduleExports <>
+               ", moduleForeign: " <> show moduleForeign <>
+               ", moduleImports: " <> show moduleImports <>
+               "})"
 
-readModule :: Foreign -> F Module
+readModule :: Foreign -> F (Module Unit)
 readModule x = do
   o <- objectProp "Module name not found" x
 
+  moduleDecls   <- traverseArrayProp "decls"   o.value readBind
   moduleExports <- traverseArrayProp "exports" o.value readIdent
   moduleForeign <- traverseArrayProp "foreign" o.value readIdent
   moduleImports <- traverseArrayProp "imports" o.value readModuleName
@@ -42,7 +53,8 @@ readModule x = do
   let moduleName = ModuleName o.key
 
   pure $ Module
-    { moduleExports: moduleExports
+    { moduleDecls: moduleDecls
+    , moduleExports: moduleExports
     , moduleForeign: moduleForeign
     , moduleImports: moduleImports
     , moduleName: moduleName
@@ -59,5 +71,5 @@ readModule x = do
     -> F (Array a)
   traverseArrayProp prop value f = readProp prop value >>= readArray >>= traverse f
 
-readModuleJSON :: String -> F Module
+readModuleJSON :: String -> F (Module Unit)
 readModuleJSON json = parseJSON json >>= readModule
